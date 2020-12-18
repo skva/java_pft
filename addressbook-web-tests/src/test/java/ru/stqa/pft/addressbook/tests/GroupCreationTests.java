57ffca5bf09d7951752d5bb63f94014e79c370f2
skva <skva1er@yandex.ru>
2020-12-19 00:19:41 +0400

    5.1 Теперь браузер открывается один раз при запуске пакета тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 public class GroupCreationTests extends TestBase{
 
-
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
