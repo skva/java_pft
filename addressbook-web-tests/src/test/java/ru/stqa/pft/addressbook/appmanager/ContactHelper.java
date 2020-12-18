aeb76029b68cd525a91bae00647c3e46758632da
skva <skva1er@yandex.ru>
2020-12-18 14:02:36 +0400

    Правки. Теперь в лист имя и фамилия попадают отдельно

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -70,12 +70,15 @@ public class ContactHelper extends HelperBase{
 
     public List<ContactData> getContactList() {
         List<ContactData> contacts = new ArrayList<ContactData>();
-        List<WebElement> elements = wd.findElements(By.xpath("//td[2]")); //считает в том числе название столбца
+        List<WebElement> elements = wd.findElements(By.name("entry"));
         for (WebElement element : elements) {
-            String name = element.getText();
-            ContactData contact = new ContactData(name, null, null, null, null);
+            //String id = element.findElement(By.tagName("input")).getAttribute("id");
+            String firstname = element.findElement(By.xpath(".//td[2]")).getText();
+            String lastname = element.findElement(By.xpath(".//td[3]")).getText();
+            ContactData contact = new ContactData(firstname, null, lastname, null, null);
             contacts.add(contact);
         }
         return contacts;
     }
 }
+
